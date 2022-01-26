13014869eb80bbf66ff341988dda235b147f500a
Asher Mancinelli <ashermancinelli@gmail.com>
2022-01-26 15:57:17 -0700

    Publish

---
+++ t.sh
@@ -0,0 +1,2 @@
+sudo codesign --remove-signature /Applications/Discord.app/Contents/Frameworks/Discord\ Helper\ \(GPU\).app /Applications/Discord.app/Contents/Frameworks/Discord\ Helper\ \(Plugin\).app /Applications/Discord.app/Contents/Frameworks/Discord\ Helper\ \(Renderer\).app /Applications/Discord.app/Contents/Frameworks/Discord\ Helper.app
+
